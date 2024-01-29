2eccd860756da7822ec0e3918271d14a8b985dcd
Buu Quoc Lam <buuquoclam@gmail.com>
2024-01-29 15:03:24 -0700

    added dotenv file

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import mongoose from "mongoose";
-const app = express();
+import { config } from "dotenv";
+config();
 
+const PORT = 5000;
+const DB_URL = "mongodb://127.0.0.1:27017/TheLams_Bakery"; // || process.env.MONGO_URL
 mongoose
-  .connect("mongodb://127.0.0.1:27017/TheLams_Bakery")
+  .connect(DB_URL)
   .then(() => {
     console.log("CONNECTED TO THELAMS_BAKERY");
   })
@@ -11,10 +14,20 @@ mongoose
     console.log("FAILED TO CONNECT TO MONGO DB", err);
   });
 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
   res.send("hello from express with nodemon setup");
 });
 
-app.listen(5000, () => {
-  console.log("APP IS LISTENNING 5000");
+app.post("/bread", (req: Request, res: Response) => {
+  const { ingredient, type, dairy } = req.body;
+  console.log(`request body: ${req.body}`);
+  res.send(`received post request: ${ingredient} ${type} ${dairy}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`APP IS LISTENNING ${PORT}`);
 });
