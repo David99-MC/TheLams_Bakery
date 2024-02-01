659b5e2bb307720adff02f30f2d14446e6aef42e
Buu Quoc Lam <buuquoclam@gmail.com>
2024-02-01 14:25:20 -0700

    Added tailwind and some basic stylings

---
+++ client/src/features/user/Username.tsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold">Welcome, Buu!</div>
+}
+
+export default Username
--- client/src/ui/Header.tsx
+++ client/src/ui/Header.tsx
@@ -1,11 +1,18 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router-dom"
+import Username from "../features/user/Username"
 
 function Header() {
   return (
-    <header>
-      <Link to="/">The Lam's Bakery</Link>
+    <header className="bg-yellow-500">
+      <Link className="uppercase tracking-widest" to="/">
+        The Lam's Bakery
+      </Link>
+      <br />
+      <input type="text" placeholder="Search product" />
+
+      <Username />
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
