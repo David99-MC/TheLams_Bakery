74a5fc8520345efbafc9eda991ff6f5d201aa46e
Buu Quoc Lam <buuquoclam@gmail.com>
2024-03-01 14:23:00 -0700

    miscellaneous

--- server/middleware/jwtVerifyMiddleware.ts
+++ server/middleware/jwtVerifyMiddleware.ts
@@ -3,7 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import asyncHandler from "express-async-handler";
 
 import { config } from "dotenv";
-import type { cartItemType } from "../src/models/user";
 config();
 
 // Ref: https://www.linkedin.com/pulse/working-typescript-express-tim-kent/
@@ -43,32 +42,3 @@ export const verifyJWT = asyncHandler(
     );
   }
 );
-
-////// Original code
-// const token = req.cookies["jwt"];
-// if (token) {
-//   try {
-//     const decoded = jwt.verify(
-//       token,
-//       process.env.JWT_SECRET as Secret
-//     ) as JwtPayload;
-
-//     const foundUser = await User.findById(decoded.userId).select(
-//       "-password"
-//     );
-
-//     // attach the user to the request object for frontend
-//     req.user = {
-//       userId: foundUser?._id,
-//       username: foundUser?.username || "",
-//     };
-//     next();
-//   } catch (error) {
-//     res.status(401);
-//     throw new Error("Not authorized, invalid token");
-//   }
-// }
-// if (!token) {
-//   res.status(401);
-//   throw new Error("Not authorized, no token");
-// }
