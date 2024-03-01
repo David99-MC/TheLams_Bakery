472243a1451b65ae6e5d1bb07e33c1eecb74bc8c
Buu Quoc Lam <buuquoclam@gmail.com>
2024-03-01 14:42:10 -0700

    Changed allowed origins to only accept request from our site

--- server/config/allowedOrigins.ts
+++ server/config/allowedOrigins.ts
@@ -1,6 +1,1 @@
-export const allowedOrigins = [
-  "https://www.mysite.com",
-  "http://127.0.0.1:5173",
-  "http://localhost:5173",
-  "http://localhost:5000",
-];
+export const allowedOrigins = ["https://the-lams-bakery.onrender.com"];
--- server/config/corsOptions.ts
+++ server/config/corsOptions.ts
@@ -2,6 +2,7 @@ import { allowedOrigins } from "./allowedOrigins";
 
 export const corsOptions = {
   origin: (origin, callback) => {
+    // !origin means the request is coming from services like Postman or Insomnia
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
